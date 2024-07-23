b5aed431e119cc5baf85bda4600b167136020aad
starllll <ankky699@gmail.com>
2024-07-23 22:49:33 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git openappfilter https://github.com/destan19/OpenAppFilter.git' >>feeds.conf.default
-#echo 'src-git passwallpackages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
-#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
+#echo 'src-git openappfilter https://github.com/destan19/OpenAppFilter.git' >>feeds.conf.default
+echo 'src-git passwallpackages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
